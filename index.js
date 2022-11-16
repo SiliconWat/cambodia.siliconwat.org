7a3e0f8b90747251d116ff896dac0e5bb99782a6
Thon Ly <thonly@heartbank.ceo>
2022-11-15 16:39:12 -0800

    refactor

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ import "./components/sw-header/element.mjs";
 import "./components/sw-main/sw-learn/element.mjs";
 import "./components/sw-main/sw-practice/element.mjs";
 import "./components/sw-main/sw-review/element.mjs";
-import "./components/sw-main/sw-home/element.mjs";
-import "./components/sw-main/element.mjs";
+//import "./components/sw-main/element.mjs";
 
 import "./components/sw-footer/element.mjs";
 import { FRONTEND } from "/global.mjs";
 
 window.onload = async () => {
+    await import(`${FRONTEND}/components/sw-main/sw-home/element.mjs`);
+    await import(`/components/sw-main/element.mjs`);
+
     await import(`${FRONTEND}/components/sw-progress/element.mjs`);
     await import(`${FRONTEND}/components/sw-music/element.mjs`);
 }
