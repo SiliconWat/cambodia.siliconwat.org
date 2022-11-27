d1fa174b4f9facb00d3ca1049bbd9dc3d6036c7f
Thon Ly <thonly@heartbank.ceo>
2022-11-27 12:12:59 -0800

    refactor

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ import "/components/sw-footer/element.mjs";
 import { FRONTEND } from "/global.mjs";
 
 window.onload = async () => {
-    const { getGitHub } = await import(`${FRONTEND}/global.mjs`);
     await import(`${FRONTEND}/components/sw-auth/element.mjs`);
 
     await import(`${FRONTEND}/components/sw-main/sw-learn/element.mjs`);
@@ -21,12 +20,13 @@ window.onload = async () => {
     await import(`${FRONTEND}/components/sw-progress/element.mjs`);
     await import(`${FRONTEND}/components/sw-music/element.mjs`);
 
+    const { getGitHub } = await import(`${FRONTEND}/global.mjs`);
     const github = await getGitHub();
-    await document.querySelector('sw-header').render(github);
     await document.querySelector('sw-main').render(github);
-    await document.querySelector('sw-progress').render(github);
-    document.documentElement.style.background = "linear-gradient(90deg, rgba(5,117,230,1) 0%, rgba(2,27,121,1) 100%)";
+    document.documentElement.style.backgroundImage = "linear-gradient(90deg, rgba(5,117,230,1) 0%, rgba(2,27,121,1) 100%)";
     document.body.style.display = 'flex';
+    await document.querySelector('sw-header').render(github);
+    await document.querySelector('sw-progress').render(github);
 };
 
 window.dataLayer = window.dataLayer || [];
